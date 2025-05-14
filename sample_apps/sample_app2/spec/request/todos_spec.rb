6d514c4b6252e4f3e027db4cd4eab9e5d751a99d
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2025-05-14 20:57:06 +0200

    add first spec

--- sample_apps/sample_app2/spec/request/todos_spec.rb
+++ sample_apps/sample_app2/spec/request/todos_spec.rb
@@ -1,4 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
 describe "Todos" do
+  describe "POST /todos" do
+    context "with valid parameters" do
+      it "creates a new todo" do
+        post '/todos', {
+          title: "Test Todo",
+          description: "This is a test todo"
+        }
 
-
+        expect(last_response.status).to eq(201)
+        
+        json_response = JSON.parse(last_response.body)
+        expect(json_response['message']).to eq("Todo created")
+        expect(json_response['todo']['title']).to eq("Test Todo")
+        expect(json_response['todo']['description']).to eq("This is a test todo")
+        expect(json_response['todo']['completed']).to eq(false)
+      end
+    end
+    
+    context "with invalid parameters" do
+      it "returns validation errors when title is missing" do
+        post '/todos', {
+          description: "This todo has no title"
+        }
+        
+        expect(last_response.status).to eq(422)
+        
+        json_response = JSON.parse(last_response.body)
+        expect(json_response['error']).to eq("Validation failed")
+        expect(json_response['details']).to have_key('title')
+      end
+      
+      it "returns validation errors when title is too long" do
+        post '/todos', {
+          title: "X" * 101,
+          description: "This todo has a title that is too long"
+        }
+        
+        expect(last_response.status).to eq(422)
+        
+        json_response = JSON.parse(last_response.body)
+        expect(json_response['error']).to eq("Validation failed")
+        expect(json_response['details']).to have_key('title')
+      end
+    end
+  end
 end
--- sample_apps/sample_app2/spec/spec_helper.rb
+++ sample_apps/sample_app2/spec/spec_helper.rb
@@ -1,1 +1,17 @@
+# frozen_string_literal: true
+
 require "rspec"
+require "rack/test"
+require "json"
+
+# Load the application
+require_relative '../app'
+
+# Configure RSpec
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
+  def app
+    TodoApp.app
+  end
+end
