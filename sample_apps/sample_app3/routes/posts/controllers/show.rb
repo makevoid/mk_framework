4cad79162e5c3bea44bfeefd1107a30ef739633d
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2025-05-16 13:13:57 +0200

    update show

--- sample_apps/sample_app3/routes/posts/controllers/show.rb
+++ sample_apps/sample_app3/routes/posts/controllers/show.rb
@@ -3,12 +3,10 @@
 class PostsShowController < MK::Controller
   route do |r|
     post = Post[r.params.fetch('id')]
-    
+
     if post
-      # Load comments for the post
       comments = Comment.where(post_id: post.id).all
-      
-      # Create a hash with post and comments
+
       {
         post: post,
         comments: comments
