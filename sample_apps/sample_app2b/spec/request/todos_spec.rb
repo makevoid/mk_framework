be07f122f0a25caef4493d80c94236cfb60197b1
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2025-05-17 09:50:27 +0200

    update todos

--- sample_apps/sample_app2b/spec/request/todos_spec.rb
+++ sample_apps/sample_app2b/spec/request/todos_spec.rb
@@ -45,8 +45,6 @@ describe "Todos" do
 
       @todo = Todo.create(
         title: "Test Todo",
-        description: "This is a test todo",
-        completed: false
       )
     end
 
@@ -58,8 +56,6 @@ describe "Todos" do
 
         expect(resp[:id]).to eq @todo.id
         expect(resp[:title]).to eq "Test Todo"
-        expect(resp[:description]).to eq "This is a test todo"
-        expect(resp[:completed]).to eq false
       end
     end
 
@@ -78,15 +74,12 @@ describe "Todos" do
       it "creates a new todo" do
         post '/todos', {
           title: "Test Todo",
-          description: "This is a test todo"
         }
 
         expect(last_response.status).to eq 201
 
         expect(resp[:message]).to eq "Todo created"
         expect(resp[:todo][:title]).to eq "Test Todo"
-        expect(resp[:todo][:description]).to eq "This is a test todo"
-        expect(resp[:todo][:completed]).to eq false
       end
     end
 
@@ -105,7 +98,6 @@ describe "Todos" do
       it "returns validation errors when title is too long" do
         post '/todos', {
           title: "X" * 101,
-          description: "This todo has a title that is too long"
         }
 
         expect(last_response.status).to eq 422
@@ -122,8 +114,6 @@ describe "Todos" do
 
       @todo = Todo.create(
         title: "Original Title",
-        description: "Original Description",
-        completed: false
       )
     end
 
@@ -138,13 +128,11 @@ describe "Todos" do
         expect(resp[:message]).to eq "Todo updated"
         expect(resp[:todo][:id]).to eq @todo.id
         expect(resp[:todo][:title]).to eq "Updated Title"
-        expect(resp[:todo][:description]).to eq "Original Description"
-        expect(resp[:todo][:completed]).to eq false
       end
 
       it "updates the todo description" do
         post "/todos/#{@todo.id}", {
-          description: "Updated Description"
+          title: "123"
         }
 
         expect(last_response.status).to eq 200
@@ -152,8 +140,6 @@ describe "Todos" do
         expect(resp[:message]).to eq "Todo updated"
         expect(resp[:todo][:id]).to eq @todo.id
         expect(resp[:todo][:title]).to eq "Original Title"
-        expect(resp[:todo][:description]).to eq "Updated Description"
-        expect(resp[:todo][:completed]).to eq false
       end
 
       it "updates the todo completed status" do
@@ -166,15 +152,11 @@ describe "Todos" do
         expect(resp[:message]).to eq "Todo updated"
         expect(resp[:todo][:id]).to eq @todo.id
         expect(resp[:todo][:title]).to eq "Original Title"
-        expect(resp[:todo][:description]).to eq "Original Description"
-        expect(resp[:todo][:completed]).to eq true
       end
 
       it "updates multiple fields at once" do
         post "/todos/#{@todo.id}", {
           title: "Completely Updated",
-          description: "New Description",
-          completed: true
         }
 
         expect(last_response.status).to eq 200
@@ -182,19 +164,6 @@ describe "Todos" do
         expect(resp[:message]).to eq "Todo updated"
         expect(resp[:todo][:id]).to eq @todo.id
         expect(resp[:todo][:title]).to eq "Completely Updated"
-        expect(resp[:todo][:description]).to eq "New Description"
-        expect(resp[:todo][:completed]).to eq true
-      end
-
-      it "returns validation errors when title is too long" do
-        post "/todos/#{@todo.id}", {
-          title: "X" * 101
-        }
-
-        expect(last_response.status).to eq 400
-
-        expect(resp[:error]).to eq "Validation failed!"
-        expect(resp[:details]).to have_key :title
       end
     end
 
@@ -216,8 +185,6 @@ describe "Todos" do
 
       @todo = Todo.create(
         title: "Todo to Delete",
-        description: "This todo will be deleted",
-        completed: false
       )
     end
 
@@ -230,8 +197,6 @@ describe "Todos" do
         expect(resp[:message]).to eq "Todo deleted successfully"
         expect(resp[:todo][:id]).to eq @todo.id
         expect(resp[:todo][:title]).to eq "Todo to Delete"
-        expect(resp[:todo][:description]).to eq "This todo will be deleted"
-        expect(resp[:todo][:completed]).to eq false
 
         # Verify that the todo was actually deleted from the database
         expect(Todo[@todo.id]).to be_nil
